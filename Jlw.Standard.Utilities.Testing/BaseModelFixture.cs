819fdc1f0d5fddc2d171a734f4f80ac3e3423c73
Jason Walker <jlwalker@spsmail.org>
2020-07-30 11:12:21 -0500

    Added BaseModelFixture for testing model members

---
+++ Jlw.Standard.Utilities.Testing/BaseModelFixture.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Jlw.Standard.Utilities.Testing
+{
+
+    class BaseModelFixture
+    {
+        public object GetPublicPropertyValueByName(object o, string sMemberName)
+        {
+            var p = typeof(object).GetProperty(sMemberName);
+            Assert.IsNotNull(p, $"Object does not contain a propery with the name '{sMemberName}'");
+
+            p = typeof(object).GetProperty(sMemberName, BindingFlags.Public);
+            Assert.IsNotNull(p, $"Object does not contain a Public member with the name '{sMemberName}'");
+
+            Assert.IsTrue(p.CanRead, $"Object does not contain a readable member with the name '{sMemberName}'");
+            
+            return p.GetValue(o);
+        }
+
+        public void AssertTypeForObjectPublicMemberModel(object o, string sMemberName, Type type)
+        {
+            Assert.IsInstanceOfType(type, typeof(Type), "type argument cannot be null");
+
+            var p = GetPublicPropertyValueByName(o, sMemberName);
+            Assert.IsInstanceOfType(p, type);
+        }
+    }
+}
