dfccf766588bc7b0c901ad1273aa0a348b799343
Jason Walker <jlwalker@spsmail.org>
2020-07-30 12:34:39 -0500

    added helper asserts

--- Jlw.Standard.Utilities.Testing/BaseModelFixture.cs
+++ Jlw.Standard.Utilities.Testing/BaseModelFixture.cs
@@ -9,25 +9,55 @@ namespace Jlw.Standard.Utilities.Testing
 
     public class BaseModelFixture<TModel>
     {
-        public object GetPublicPropertyValueByName(TModel o, string sMemberName)
+        public PropertyInfo GetPropertyInfoByName(string sMemberName, BindingFlags flags = BindingFlags.Default)
         {
-            var p = typeof(TModel).GetProperty(sMemberName);
-            Assert.IsNotNull(p, $"Object does not contain a propery with the name '{sMemberName}'");
+            return typeof(TModel).GetProperty(sMemberName, flags);
 
-            p = typeof(TModel).GetProperty(sMemberName, BindingFlags.Public);
-            Assert.IsNotNull(p, $"Object does not contain a Public member with the name '{sMemberName}'");
+        }
 
-            Assert.IsTrue(p.CanRead, $"Object does not contain a readable member with the name '{sMemberName}'");
-            
+        public PropertyInfo AssertGetPropertyInfoByName(string sMemberName, BindingFlags flags = BindingFlags.Default)
+        {
+            var p = GetPropertyInfoByName(sMemberName, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+            Assert.IsNotNull(p, $"{typeof(TModel)} does not contain a property with the name '{sMemberName}'");
+
+            p = GetPropertyInfoByName(sMemberName, flags);
+            Assert.IsNotNull(p, $"{typeof(TModel)} does not contain a member with the name '{sMemberName}', and with the Binding Flags: {flags}");
+
+            return p;
+        }
+
+        public void AssertPropertyIsReadable(string sMemberName)
+        {
+            var p = GetPropertyInfoByName(sMemberName, BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+            Assert.IsNotNull(p, $"{typeof(TModel)} does not contain a property with the name '{sMemberName}'");
+
+            Assert.IsTrue(p.CanRead, $"{sMemberName} is not a readable property.");
+        }
+
+
+
+        public object AssertGetPublicPropertyValueByName(TModel o, string sMemberName)
+        {
+            // Assert member exists at all?
+
+            // Assert member is public
+            var p = GetPropertyInfoByName(sMemberName, BindingFlags.Instance | BindingFlags.Public);
+            Assert.IsNotNull(p, $"{typeof(TModel)} does not contain a public member with the name '{sMemberName}'.");
+
+            AssertPropertyIsReadable(sMemberName);
+
             return p.GetValue(o);
         }
 
         public void AssertTypeForObjectPublicMemberModel(TModel o, string sMemberName, Type type)
         {
-            Assert.IsInstanceOfType(type, typeof(Type), "type argument cannot be null");
+            Assert.IsNotNull(type, "type argument cannot be null");
 
-            var p = GetPublicPropertyValueByName(o, sMemberName);
+            Assert.IsInstanceOfType(type, typeof(Type));
+
+            var p = AssertGetPublicPropertyValueByName(o, sMemberName);
             Assert.IsInstanceOfType(p, type);
+
         }
     }
 }
