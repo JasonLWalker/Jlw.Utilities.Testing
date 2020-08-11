06b88e1ba216f963dec68ebceaa2ea1d141904b2
Jason Walker <jlwalker@spsmail.org>
2020-08-11 10:16:24 -0500

    Added tests for Kvp Int16 data source

---
+++ Jlw.Standard.Utilities.Testing.Tests/UnitTests/DataSourceTests/KvpObjectInt16SourceAttributeFixture.cs
@@ -0,0 +1,45 @@
+using System;
+using Jlw.Standard.Utilities.Data;
+using Jlw.Standard.Utilities.Testing.DataSources;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Jlw.Standard.Utilities.Testing.Tests.UnitTests.DataSourceTests
+{
+    [TestClass]
+    public class KvpObjectInt16SourceAttributeFixture
+    {
+
+        [TestMethod]
+        [KvpObjectInt16Source]
+        public void Should_BeInstanceOf_NullableObject_ForFirstArgument(object o, object b)
+        {
+            if (o != null)
+                Assert.IsInstanceOfType(o, typeof(object));
+        }
+
+        [TestMethod]
+        [KvpObjectInt16Source]
+        public void Should_BeInstanceOf_Int16_ForSecondArgument(object o, object b)
+        {
+            Assert.IsNotNull(b);
+            Assert.IsInstanceOfType(b, typeof(Int16));
+        }
+
+        [TestMethod]
+        [KvpObjectInt16Source]
+        public void Should_ParseAs_Int16(object o, object b)
+        {
+            Assert.IsInstanceOfType(DataUtility.ParseAs(typeof(Int16), o), typeof(Int16));
+        }
+
+        [TestMethod]
+        [KvpObjectInt16Source]
+        public void Should_Match_ForParsedValue(object o, object b)
+        {
+            Int16 expected = (Int16)(b ?? false);
+            Assert.AreEqual(expected, DataUtility.ParseAs(typeof(Int16), o));
+        }
+
+
+    }
+}
