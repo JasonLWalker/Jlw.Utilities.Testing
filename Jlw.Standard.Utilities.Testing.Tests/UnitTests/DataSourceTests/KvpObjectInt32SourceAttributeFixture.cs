ade342f5c38f835d496a1b6a3259fae88fd1e205
Jason Walker <jlwalker@spsmail.org>
2020-08-11 10:16:45 -0500

    Added tests for Kvp Int32 data source

---
+++ Jlw.Standard.Utilities.Testing.Tests/UnitTests/DataSourceTests/KvpObjectInt32SourceAttributeFixture.cs
@@ -0,0 +1,45 @@
+using System;
+using Jlw.Standard.Utilities.Data;
+using Jlw.Standard.Utilities.Testing.DataSources;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Jlw.Standard.Utilities.Testing.Tests.UnitTests.DataSourceTests
+{
+    [TestClass]
+    public class KvpObjectInt32SourceAttributeFixture
+    {
+
+        [TestMethod]
+        [KvpObjectInt32Source]
+        public void Should_BeInstanceOf_NullableObject_ForFirstArgument(object o, object b)
+        {
+            if (o != null)
+                Assert.IsInstanceOfType(o, typeof(object));
+        }
+
+        [TestMethod]
+        [KvpObjectInt32Source]
+        public void Should_BeInstanceOf_Int32_ForSecondArgument(object o, object b)
+        {
+            Assert.IsNotNull(b);
+            Assert.IsInstanceOfType(b, typeof(Int32));
+        }
+
+        [TestMethod]
+        [KvpObjectInt32Source]
+        public void Should_ParseAs_Int32(object o, object b)
+        {
+            Assert.IsInstanceOfType(DataUtility.ParseAs(typeof(Int32), o), typeof(Int32));
+        }
+
+        [TestMethod]
+        [KvpObjectInt32Source]
+        public void Should_Match_ForParsedValue(object o, object b)
+        {
+            Int32 expected = (Int32)(b ?? false);
+            Assert.AreEqual(expected, DataUtility.ParseAs(typeof(Int32), o));
+        }
+
+
+    }
+}
