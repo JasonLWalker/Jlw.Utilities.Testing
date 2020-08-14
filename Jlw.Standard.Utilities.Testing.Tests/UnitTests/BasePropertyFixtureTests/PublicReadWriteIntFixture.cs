14abe75e060c674182e7d77b15dd7113dc72c2aa
Jason Walker <jlwalker@spsmail.org>
2020-08-14 10:35:05 -0500

    Added fixture to test BasePropertyFixture

---
+++ Jlw.Standard.Utilities.Testing.Tests/UnitTests/BasePropertyFixtureTests/PublicReadWriteIntFixture.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Jlw.Standard.Utilities.Testing.Tests.UnitTests.BasePropertyFixtureTests
+{
+    [TestClass]
+    public class PublicReadWriteIntFixture : BasePropertyFixture<SampleModelForTesting, int>
+    {
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext ctx)
+        {
+            PropertyName = "PublicReadWriteInt";
+        }
+
+        [TestMethod]
+        [DataRow(typeof(int))]
+        public override void Should_BeInstanceOf(Type t)
+        {
+            base.Should_BeInstanceOf(t);
+        }
+
+        [TestMethod]
+        [DataRow(MethodAttributes.Public)]
+        public override void Should_MatchAccessScope_ForGet(MethodAttributes attr)
+        {
+            base.Should_MatchAccessScope_ForGet(attr);
+        }
+
+        [TestMethod]
+        [DataRow(MethodAttributes.Public)]
+        public override void Should_MatchAccessScope_ForSet(MethodAttributes attr)
+        {
+            base.Should_MatchAccessScope_ForSet(attr);
+        }
+
+        [TestMethod]
+        public void Should_MatchValueFor_DefaultConstructor()
+        {
+            var o = new SampleModelForTesting();
+            base.Should_MatchValue(o, int.MinValue);
+        }
+    }
+}
