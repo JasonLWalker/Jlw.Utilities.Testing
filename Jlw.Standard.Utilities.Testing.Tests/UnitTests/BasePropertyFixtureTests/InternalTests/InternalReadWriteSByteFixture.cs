9df131908467ef4b3e3b008f2ac8dd10c6b7d7be
Jason Walker <jlwalker@spsmail.org>
2020-08-14 22:46:16 -0500

    Added internal tests

---
+++ Jlw.Standard.Utilities.Testing.Tests/UnitTests/BasePropertyFixtureTests/InternalTests/InternalReadWriteSByteFixture.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Reflection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Jlw.Standard.Utilities.Testing.Tests.UnitTests.BasePropertyFixtureTests.InternalTests
+{
+    [TestClass]
+    public class InternalReadWriteSByteFixture : BasePropertyFixture<SampleModelForTesting, SByte>
+    {
+        
+        public InternalReadWriteSByteFixture ()
+        {
+            DefaultInstance = new SampleModelForTesting();
+            PropertyName = "InternalReadWriteSByte";
+        }
+
+        [TestMethod]
+        [DataRow(AccessScope.Accessors.Internal)]
+        public override void Should_MatchAccessScope_ForGet(MethodAttributes attr)
+        {
+            base.Should_MatchAccessScope_ForGet(attr);
+        }
+
+        [TestMethod]
+        [DataRow(AccessScope.Accessors.Internal)]
+        public override void Should_MatchAccessScope_ForSet(MethodAttributes attr)
+        {
+            base.Should_MatchAccessScope_ForSet(attr);
+        }
+
+    }
+}
---
+++ Jlw.Standard.Utilities.Testing.Tests/UnitTests/BasePropertyFixtureTests/InternalTests/InternalStaticReadWriteSByteFixture.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Reflection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Jlw.Standard.Utilities.Testing.Tests.UnitTests.BasePropertyFixtureTests.InternalTests
+{
+    [TestClass]
+    public class InternalStaticReadWriteSByteFixture : BasePropertyFixture<SampleModelForTesting, SByte>
+    {
+        
+        public InternalStaticReadWriteSByteFixture ()
+        {
+            DefaultInstance = new SampleModelForTesting();
+            PropertyName = "InternalStaticReadWriteSByte";
+        }
+
+        [TestMethod]
+        [DataRow(AccessScope.Accessors.Internal | AccessScope.Static)]
+        public override void Should_MatchAccessScope_ForGet(MethodAttributes attr)
+        {
+            base.Should_MatchAccessScope_ForGet(attr);
+        }
+
+        [TestMethod]
+        [DataRow(AccessScope.Accessors.Internal | AccessScope.Static)]
+        public override void Should_MatchAccessScope_ForSet(MethodAttributes attr)
+        {
+            base.Should_MatchAccessScope_ForSet(attr);
+        }
+
+    }
+}
